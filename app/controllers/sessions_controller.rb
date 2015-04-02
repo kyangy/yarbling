6c0e66faf9c4808ca464e0fc1bf38d7f0bab9e90
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-02 12:23:36 -0700

    added sessions methods

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,25 @@
 class SessionsController < ApplicationController
+
+	def new
+	end
+
+	def create
+	  user = User.where(email: params[:login][:email]).first
+	  if user && user.authenticate(params[:login][:password])
+	    log_in(user)
+	    redirect_to root_path
+	    # if user.admin == true
+	    #   redirect_to pickups_path(user)
+	    # else
+	    #  redirect_to user_path(user)
+	    # end
+	  else
+	    redirect_to login_path
+	  end
+	end
+
+	def destroy
+	  log_out if logged_in?
+	  redirect_to root_path
+	end
 end
