e21bfbdfc7d73e94e8c95996c7fa8df662a29fe2
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-02 12:20:16 -0700

    created users new view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,38 @@
 class UsersController < ApplicationController
+
+	def new
+		@user = User.new
+		if logged_in?
+			redirect_to root_path
+		end
+	end
+
+	def create
+		@user = User.new(user_params)
+		if @user.save
+			log_in @user
+			redirect_to root_path
+		else
+			render :new
+		end
+	end
+
+	def edit
+		@user = current_user
+	end
+
+	def update
+		@user = current_user
+		if @user.update_attributes(user_params)
+			redirect_to root_path
+		else
+			render :edit
+		end
+	end
+
+	private
+
+	def user_params
+		params.require(:user).permit(:first, :last, :email, :address, :city, :state, :zip, :password, :password_confirmation)
+	end
 end
