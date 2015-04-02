4d43269a079622b840de9683e1746119ffe281da
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-02 12:32:46 -0700

    working user login

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,13 @@
 class User < ActiveRecord::Base
+
+	has_secure_password
+
+	validates :first, presence: true
+	validates :last, presence: true
+	validates :email, presence: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i }, uniqueness: {case_sensitive: false}, length: {maximum: 255}
+	validates :address, presence: true
+	validates :city, presence: true
+	validates :state, presence: true
+	validates :zip, presence: true
+	validates :password, length: { minimum: 6 }
 end
