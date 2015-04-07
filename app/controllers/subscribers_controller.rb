42646c2db19ba30a430ce2209b218edc10f7a9a8
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-06 17:43:24 -0700

    created subscribers methods

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,2 +1,21 @@
 class SubscribersController < ApplicationController
+
+	def new 
+	end
+
+	def update
+		token = params[:stripeToken]
+
+		customer = Stripe::Customer.create(
+			card: token,
+			plan: 1020,
+			email: current_user.email
+			)
+
+		current_user.subscribed = true
+		current_user.stripeid = customer.id
+		current_user.save
+
+		redirect_to root_path
+	end
 end
