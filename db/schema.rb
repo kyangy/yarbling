2f4ff30a9c94cba94d052bb77a12b7167c6eed63
Kevin Yang <YOUR-EMAIL-ADDRESS>
2015-04-06 18:19:50 -0700

    working subscriptions using stripe

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150402190407) do
+ActiveRecord::Schema.define(version: 20150407003543) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,8 @@ ActiveRecord::Schema.define(version: 20150402190407) do
     t.string   "password_digest"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+    t.boolean  "subscribed"
+    t.string   "stripeid"
   end
 
 end
